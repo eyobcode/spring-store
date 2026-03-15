131c4684da5debbd1f0cb80f037354e5a6aa4d27
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-03-16 02:43:45 +0300

    feat: create address table

---
+++ src/main/java/com/codewitheyob/store/entities/Address.java
@@ -0,0 +1,30 @@
+package com.codewitheyob.store.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "addresses")
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, name = "street")
+    private String street;
+
+    @Column(nullable = false, name = "city")
+    private String city;
+
+    @Column(nullable = false, name = "zip")
+    private String zip;
+
+    @Column(nullable = false, name = "state")
+    private String state;
+
+}
