b55fa23d79dd149d1a7b219deabfcf75c42a3656
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-03-16 02:45:21 +0300

    feat: tag user model

---
+++ src/main/java/com/codewitheyob/store/entities/Tag.java
@@ -0,0 +1,21 @@
+package com.codewitheyob.store.entities;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "tags")
+public class Tag {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false, name = "name")
+    private String name;
+}
