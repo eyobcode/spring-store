626dd62c6c9ae9e6a8c04ede0124c3345fe3bedc
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-03-16 02:45:13 +0300

    feat: create user model

---
+++ src/main/java/com/codewitheyob/store/entities/User.java
@@ -0,0 +1,29 @@
+package com.codewitheyob.store.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, name = "name")
+    private String name;
+
+    @Column(nullable = false, name = "email")
+    private String email;
+
+    @Column(nullable = false, name = "password")
+    private String password;
+
+}
+
+
