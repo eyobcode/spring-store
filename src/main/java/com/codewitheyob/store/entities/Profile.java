35698f7644b24e37faf4d7b3a3effddb8853c95a
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-03-16 02:59:32 +0300

    feat: address user model

--- src/main/java/com/codewitheyob/store/entities/Profile.java
+++ src/main/java/com/codewitheyob/store/entities/Profile.java
@@ -16,13 +16,13 @@ public class Profile {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, name = "bio")
+    @Column(name = "bio")
     private String bio;
 
-    @Column(nullable = false, name = "phone_number")
+    @Column(name = "phone_number")
     private String phone_number;
 
-    @Column(nullable = false, name = "loyalty_point")
+    @Column(name = "loyalty_point")
     private int loyalty_point;
 
 }
