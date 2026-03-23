e181153db4b7bf90a47e7230531450212d8e460b
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-03-23 22:07:24 +0300

    REFACTOR: change on-delete from cascade to restrict

---
+++ src/main/java/com/codewitheyob/store/repositories/ProfileRepository.java
@@ -0,0 +1,10 @@
+package com.codewitheyob.store.repositories;
+
+import com.codewitheyob.store.entities.Profile;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface ProfileRepository extends CrudRepository<Profile, Long> {
+    List<Profile> getById(Long id);
+}
