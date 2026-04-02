2cb79b850ab703b0313e94cb97c62d3adf5174fc
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-04-02 03:29:01 +0300

    feat(initial)

--- src/main/resources/db/migration/V3__add_profiles_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE profiles (
-    id BIGINT PRIMARY KEY,
-    bio TEXT,
-    phone_number VARCHAR(255),
-    loyalty_point INT UNSIGNED DEFAULT 0,
-    FOREIGN KEY (id) REFERENCES users(id)
-);
