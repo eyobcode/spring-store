f63096eedddcb09c92b3db913b6f4cd71ddd956a
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-04-02 03:28:53 +0300

    feat(initial)

--- src/main/java/com/codewitheyob/store/repositories/UserRepository.java
+++ src/main/java/com/codewitheyob/store/repositories/UserRepository.java
@@ -3,5 +3,9 @@ package com.codewitheyob.store.repositories;
 import com.codewitheyob.store.entities.User;
 import org.springframework.data.repository.CrudRepository;
 
+
 public interface UserRepository extends CrudRepository<User,Long> {
+
+
+
 }
