c603c366a6823131e187ced3dfe7cb1778b4ba0d
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-04-04 03:18:34 +0300

    feat: add user controller

---
+++ src/main/java/com/codewitheyob/store/controllers/UserController.java
@@ -0,0 +1,39 @@
+package com.codewitheyob.store.controllers;
+
+import com.codewitheyob.store.dtos.UserDto;
+import com.codewitheyob.store.entities.User;
+import com.codewitheyob.store.mappers.UserMapper;
+import com.codewitheyob.store.repositories.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/users")
+public class UserController {
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+
+    @GetMapping
+    public Iterable<UserDto> getAllUsers() {
+        return userRepository.findAll()
+                .stream()
+                .map(userMapper::toDto)
+                .toList();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDto > getUser(@PathVariable Long id){
+        var user = userRepository.findById(id).orElse(null);
+        if (user == null){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(userMapper.toDto(user));
+    }
+}
