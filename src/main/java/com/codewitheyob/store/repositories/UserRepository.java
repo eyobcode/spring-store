f9a6b20309a98648c5cce14cda4adb60ea98ed2b
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-04-04 03:19:57 +0300

    refactor: change crud-repository to jpa-repository

--- src/main/java/com/codewitheyob/store/repositories/UserRepository.java
+++ src/main/java/com/codewitheyob/store/repositories/UserRepository.java
@@ -1,11 +1,9 @@
 package com.codewitheyob.store.repositories;
 
 import com.codewitheyob.store.entities.User;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 
-public interface UserRepository extends CrudRepository<User,Long> {
-
-
+public interface UserRepository extends JpaRepository<User,Long> {
 
 }
