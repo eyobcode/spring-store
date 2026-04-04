60db7208a3f2f921e537155d3cfd129cf94bad6c
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-04-04 03:18:55 +0300

    feat: add user mapper

---
+++ src/main/java/com/codewitheyob/store/mappers/UserMapper.java
@@ -0,0 +1,10 @@
+package com.codewitheyob.store.mappers;
+
+import com.codewitheyob.store.dtos.UserDto;
+import com.codewitheyob.store.entities.User;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel= "spring")
+public interface UserMapper {
+    UserDto toDto(User user);
+}
