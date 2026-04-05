a4fe2ae29f2cce0975b24147b00b5c199c53bd5d
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-04-05 22:42:17 +0300

    feat: add user endpoint

--- src/main/java/com/codewitheyob/store/controllers/UserController.java
+++ src/main/java/com/codewitheyob/store/controllers/UserController.java
@@ -1,17 +1,14 @@
 package com.codewitheyob.store.controllers;
 
 import com.codewitheyob.store.dtos.UserDto;
-import com.codewitheyob.store.entities.User;
 import com.codewitheyob.store.mappers.UserMapper;
 import com.codewitheyob.store.repositories.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import java.util.Set;
 
 @RestController
 @AllArgsConstructor
@@ -21,8 +18,12 @@ public class UserController {
     private final UserMapper userMapper;
 
     @GetMapping
-    public Iterable<UserDto> getAllUsers() {
-        return userRepository.findAll()
+    public Iterable<UserDto> getAllUsers(
+            @RequestParam(required = false, defaultValue = "", name = "sort") String sortBy
+    ) {
+        if (!Set.of("name","email").contains(sortBy))
+            sortBy = "name";
+        return userRepository.findAll(Sort.by(sortBy))
                 .stream()
                 .map(userMapper::toDto)
                 .toList();
