5427bacc42c7b9bb98ed2694149d81cce02d0d85
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-04-17 00:30:18 +0300

    feat: add create user rout + dto + mapper + controller

--- src/main/java/com/codewitheyob/store/mappers/UserMapper.java
+++ src/main/java/com/codewitheyob/store/mappers/UserMapper.java
@@ -1,5 +1,6 @@
 package com.codewitheyob.store.mappers;
 
+import com.codewitheyob.store.dtos.RegisterUserRequest;
 import com.codewitheyob.store.dtos.UserDto;
 import com.codewitheyob.store.entities.User;
 import org.mapstruct.Mapper;
@@ -7,4 +8,6 @@ import org.mapstruct.Mapper;
 @Mapper(componentModel= "spring")
 public interface UserMapper {
     UserDto toDto(User user);
+
+    User toEntity(RegisterUserRequest user);
 }
