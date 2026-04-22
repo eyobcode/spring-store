5d88250325f162bb1907abe9669a3524d087c561
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-04-23 02:18:04 +0300

    feat: add change password user rout + dto + mapper + controller

--- src/main/java/com/codewitheyob/store/controllers/UserController.java
+++ src/main/java/com/codewitheyob/store/controllers/UserController.java
@@ -1,5 +1,6 @@
 package com.codewitheyob.store.controllers;
 
+import com.codewitheyob.store.dtos.ChangePasswordRequest;
 import com.codewitheyob.store.dtos.RegisterUserRequest;
 import com.codewitheyob.store.dtos.UpdateUserRequest;
 import com.codewitheyob.store.dtos.UserDto;
@@ -7,6 +8,7 @@ import com.codewitheyob.store.mappers.UserMapper;
 import com.codewitheyob.store.repositories.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -69,6 +71,19 @@ public class UserController {
         if(user == null) return ResponseEntity.notFound().build();
 
         userRepository.delete(user);
+        return ResponseEntity.noContent().build();
+    }
+    @PostMapping("/{id}/change-password")
+    public ResponseEntity<Void> changePassword(
+            @PathVariable Long id,
+            @RequestBody ChangePasswordRequest request){
+        var user = userRepository.findById(id).orElse(null);
+        if(user == null) return ResponseEntity.notFound().build();
+        if(!user.getPassword().equals(request.getOldPassword())){
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+        user.setPassword(request.getNewPassword());
+        userRepository.save(user);
         return ResponseEntity.noContent().build();
     }
 }
