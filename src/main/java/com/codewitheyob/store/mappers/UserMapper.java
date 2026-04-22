7a4b8bec67af24fa26595a2ca2f428284fa58a58
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-04-23 01:58:27 +0300

    feat: add update user rout + dto + mapper + controller

--- src/main/java/com/codewitheyob/store/mappers/UserMapper.java
+++ src/main/java/com/codewitheyob/store/mappers/UserMapper.java
@@ -1,13 +1,15 @@
 package com.codewitheyob.store.mappers;
 
 import com.codewitheyob.store.dtos.RegisterUserRequest;
+import com.codewitheyob.store.dtos.UpdateUserRequest;
 import com.codewitheyob.store.dtos.UserDto;
 import com.codewitheyob.store.entities.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel= "spring")
 public interface UserMapper {
     UserDto toDto(User user);
-
     User toEntity(RegisterUserRequest user);
+    void update(UpdateUserRequest request, @MappingTarget User user);
 }
