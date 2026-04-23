b9c1e2e7e44b0a4359145bcaca00d64632bb6fe3
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-04-23 17:23:14 +0300

    feat: add product rout + dto + mapper + controller

--- src/main/java/com/codewitheyob/store/mappers/ProductMapper.java
+++ src/main/java/com/codewitheyob/store/mappers/ProductMapper.java
@@ -4,9 +4,13 @@ import com.codewitheyob.store.dtos.ProductDto;
 import com.codewitheyob.store.entities.Product;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public interface ProductMapper {
     @Mapping(source = "category.id", target = "categoryId")
     ProductDto toDto(Product product);
+    Product toEntity(ProductDto request);
+    @Mapping(target = "id", ignore = true)
+    void update(ProductDto productDto, @MappingTarget Product product);
 }
