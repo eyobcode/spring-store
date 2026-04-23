a0cb7eb4913a0cff2751ab39d9ccd2832a19231c
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-04-23 17:28:20 +0300

    feat: add delete product

--- src/main/java/com/codewitheyob/store/controllers/ProductController.java
+++ src/main/java/com/codewitheyob/store/controllers/ProductController.java
@@ -64,4 +64,12 @@ public class ProductController {
         productDto.setId(product.getId());
         return ResponseEntity.ok(productDto);
     }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable Long id){
+        var product = productRepository.findById(id).orElse(null);
+        if(product == null) return ResponseEntity.notFound().build();
+
+        productRepository.delete(product);
+        return ResponseEntity.noContent().build();
+    }
 }
