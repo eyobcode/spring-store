fa1ca0f16b554674daf76d2c104f9c7b03097fd6
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-04-23 17:23:10 +0300

    feat: add product rout + dto + mapper + controller

--- src/main/java/com/codewitheyob/store/dtos/ProductDto.java
+++ src/main/java/com/codewitheyob/store/dtos/ProductDto.java
@@ -2,11 +2,13 @@ package com.codewitheyob.store.dtos;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.math.BigDecimal;
 
 @AllArgsConstructor
 @Getter
+@Setter
 public class ProductDto {
     private Long id;
     private String name;
