0dd4dad97bd579d90becb2282f608dd86e2a0438
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-04-29 23:46:00 +0300

    feat: create dto + logic + mapper for cart and its item

--- src/main/java/com/codewitheyob/store/entities/CartItem.java
+++ src/main/java/com/codewitheyob/store/entities/CartItem.java
@@ -8,6 +8,8 @@ import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
 
+import java.math.BigDecimal;
+
 @Getter
 @Setter
 @Entity
@@ -28,6 +30,10 @@ public class CartItem {
 
     @Column(name = "quantity")
     private Integer quantity;
+
+    public BigDecimal getTotalPrice(){
+        return product.getPrice().multiply(BigDecimal.valueOf(quantity));
+    }
 
 
 }
