cbb8dca0444e1e48bcc0957b46888f4ef22c7141
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-04-29 23:46:07 +0300

    feat: create dto + logic + mapper for cart and its item

---
+++ src/main/java/com/codewitheyob/store/mappers/CartMapper.java
@@ -0,0 +1,15 @@
+package com.codewitheyob.store.mappers;
+
+import com.codewitheyob.store.dtos.CartDto;
+import com.codewitheyob.store.dtos.CartItemDto;
+import com.codewitheyob.store.entities.Cart;
+import com.codewitheyob.store.entities.CartItem;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface CartMapper {
+    CartDto toDto(Cart cart);
+    @Mapping(target = "totalPrice", expression = "java(cartItem.getTotalPrice())")
+    CartItemDto toDto(CartItem cartItem);
+}
