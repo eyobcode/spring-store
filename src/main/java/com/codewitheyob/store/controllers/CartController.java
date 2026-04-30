cb7bfd392cd372b5e6c39043f9bd5d353dcb1ded
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-04-30 14:00:28 +0300

    refactor: apply information expert principle

--- src/main/java/com/codewitheyob/store/controllers/CartController.java
+++ src/main/java/com/codewitheyob/store/controllers/CartController.java
@@ -2,7 +2,6 @@ package com.codewitheyob.store.controllers;
 
 import com.codewitheyob.store.dtos.*;
 import com.codewitheyob.store.entities.Cart;
-import com.codewitheyob.store.entities.CartItem;
 import com.codewitheyob.store.mappers.CartMapper;
 import com.codewitheyob.store.repositories.CartRepository;
 import com.codewitheyob.store.repositories.ProductRepository;
@@ -47,21 +46,7 @@ public class CartController {
 
         var product = productRepository.findById(request.getProductId()).orElse(null);
         if(product == null) return ResponseEntity.badRequest().build();
-        var cartItem = cart.getItems()
-                .stream()
-                .filter(item -> item.getProduct().getId().equals(product.getId()))
-                .findFirst()
-                .orElse(null);
-
-        if (cartItem != null){
-            cartItem.setQuantity(cartItem.getQuantity() + 1);
-        }else{
-            cartItem = new CartItem();
-            cartItem.setProduct(product);
-            cartItem.setQuantity(1);
-            cartItem.setCart(cart);
-            cart.getItems().add(cartItem);
-        }
+        var cartItem = cart.addItem(product);
         cartRepository.save(cart);
         var cartItemDto = cartMapper.toDto(cartItem);
 
@@ -89,14 +74,10 @@ public class CartController {
                 Map.of("error","Cart not found.")
         );
 
-        var cartItem = cart.getItems()
-                .stream()
-                .filter(item -> item.getProduct().getId().equals(productId))
-                .findFirst()
-                .orElse(null);
+        var cartItem = cart.getItem(productId);
         if(cartItem == null) return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
                 Map.of("error","Product wasn't found in the cart.")
-        );;
+        );
 
         cartItem.setQuantity(request.getQuantity());
         cartRepository.save(cart);
