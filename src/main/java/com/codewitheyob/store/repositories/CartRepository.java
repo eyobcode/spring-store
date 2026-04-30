2921bacaded24ded0159a39614043392976aac36
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-04-30 03:03:18 +0300

    feat: create dto + logic + mapper for cart and its item

--- src/main/java/com/codewitheyob/store/repositories/CartRepository.java
+++ src/main/java/com/codewitheyob/store/repositories/CartRepository.java
@@ -1,10 +1,21 @@
 package com.codewitheyob.store.repositories;
 
 import com.codewitheyob.store.entities.Cart;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface CartRepository extends JpaRepository<Cart, UUID> {
     boolean existsCartById(UUID id);
+
+    List<Cart> findCartById(UUID id);
+
+    @EntityGraph(attributePaths = "items.product")
+    @Query("SELECT c FROM Cart c WHERE c.id = :cartId")
+    Optional<Cart> getCartWithId(@Param("cartId") UUID cartId);
 }
