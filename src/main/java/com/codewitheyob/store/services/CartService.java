0b8c7d79fd3791877f2be3e6ebca90c8dae2fd8d
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-05-01 12:56:07 +0300

    refactor: add new service layer for business logic and also create error handler for cart and product not found

--- src/main/java/com/codewitheyob/store/services/CartService.java
+++ src/main/java/com/codewitheyob/store/services/CartService.java
@@ -1,7 +1,45 @@
 package com.codewitheyob.store.services;
 
+import com.codewitheyob.store.dtos.CartDto;
+import com.codewitheyob.store.dtos.CartItemDto;
+import com.codewitheyob.store.entities.Cart;
+import com.codewitheyob.store.exceptions.CartNotFoundException;
+import com.codewitheyob.store.exceptions.ProductNotFoundException;
+import com.codewitheyob.store.mappers.CartMapper;
+import com.codewitheyob.store.repositories.CartRepository;
+import com.codewitheyob.store.repositories.ProductRepository;
+import org.antlr.v4.runtime.misc.LogManager;
+import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 public class CartService {
+    private CartMapper cartMapper;
+    private CartRepository cartRepository;
+    ProductRepository productRepository;
+
+
+    public CartDto createCart(){
+
+        var cart = new Cart();
+        cartRepository.save(cart);
+
+        return cartMapper.toDto(cart);
+    }
+
+    public CartItemDto addToCart(UUID cartId,Long productId){
+        var cart = cartRepository.getCartWithId(cartId).orElse(null);
+        if (cart == null) throw new CartNotFoundException();
+
+        var product = productRepository.findById(productId).orElse(null);
+        if(product == null) throw new ProductNotFoundException();
+
+        var cartItem = cart.addItem(product);
+        cartRepository.save(cart);
+
+        return cartMapper.toDto(cartItem);
+    }
 }
