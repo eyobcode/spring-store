deb2d11b135745fbfcb1cd5717344ea494ee9633
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-05-03 07:29:06 +0300

    write login logic with spring security

---
+++ src/main/java/com/codewitheyob/store/controllers/AuthController.java
@@ -0,0 +1,34 @@
+package com.codewitheyob.store.controllers;
+
+import com.codewitheyob.store.dtos.LoginRequest;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/auth")
+public class AuthController {
+    private final AuthenticationManager authenticationManager;
+
+    @PostMapping("/login")
+    public ResponseEntity<?> loginUser(@Valid @RequestBody LoginRequest request){
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+        return ResponseEntity.ok().build();
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<Void> handleBadCredentialExceptions(){
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+    }
+}
