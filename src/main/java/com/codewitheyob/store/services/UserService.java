a43770ae65825ce36ff137b6572d4db94cc9d046
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-05-03 07:29:19 +0300

    write login logic with spring security

---
+++ src/main/java/com/codewitheyob/store/services/UserService.java
@@ -0,0 +1,30 @@
+package com.codewitheyob.store.services;
+
+import com.codewitheyob.store.repositories.UserRepository;
+import lombok.AllArgsConstructor;
+import org.jspecify.annotations.NullMarked;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@AllArgsConstructor
+@Service
+public class UserService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    @NullMarked
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        var user = userRepository.findByEmail(email).orElseThrow(
+                () -> new UsernameNotFoundException("User not found"));
+        return new User(
+                user.getEmail(),
+                user.getPassword(),
+                Collections.emptyList()
+        );
+    }
+}
