69204dfee24437e86afc07b180e8dc957a52a95d
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-05-03 07:29:09 +0300

    write login logic with spring security

---
+++ src/main/java/com/codewitheyob/store/dtos/LoginRequest.java
@@ -0,0 +1,14 @@
+package com.codewitheyob.store.dtos;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class LoginRequest {
+    @NotBlank(message = "Email is required.")
+    String email;
+
+    @NotBlank(message = "Password is required.")
+    String password;
+}
