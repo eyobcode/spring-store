843ac6a9a57db56eaa6f31449ecc63903a6f070d
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-05-03 07:29:16 +0300

    write login logic with spring security

--- src/main/java/com/codewitheyob/store/repositories/UserRepository.java
+++ src/main/java/com/codewitheyob/store/repositories/UserRepository.java
@@ -1,10 +1,15 @@
 package com.codewitheyob.store.repositories;
 
 import com.codewitheyob.store.entities.User;
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
 
 
 public interface UserRepository extends JpaRepository<User,Long> {
 
     boolean existsByEmail(String email);
+
+    Optional<User> findByEmail(String email);
 }
