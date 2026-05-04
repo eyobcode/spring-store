28ac90be622fddc978bf8fbaa0deb211e204a671
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-05-04 22:45:55 +0300

    feat: add jwt service to manage token generation and integrate with our old auth controller.

--- src/main/java/com/codewitheyob/store/controllers/AuthController.java
+++ src/main/java/com/codewitheyob/store/controllers/AuthController.java
@@ -1,6 +1,8 @@
 package com.codewitheyob.store.controllers;
 
+import com.codewitheyob.store.dtos.JwtResponse;
 import com.codewitheyob.store.dtos.LoginRequest;
+import com.codewitheyob.store.services.JwtService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -15,16 +17,18 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/auth")
 public class AuthController {
     private final AuthenticationManager authenticationManager;
+    private final JwtService jwtService;
 
     @PostMapping("/login")
-    public ResponseEntity<?> loginUser(@Valid @RequestBody LoginRequest request){
+    public ResponseEntity<JwtResponse> loginUser(@Valid @RequestBody LoginRequest request){
         authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(
                         request.getEmail(),
                         request.getPassword()
                 )
         );
-        return ResponseEntity.ok().build();
+        var token = jwtService.generateToken(request.getEmail());
+        return ResponseEntity.ok(new JwtResponse(token));
     }
 
     @ExceptionHandler(BadCredentialsException.class)
