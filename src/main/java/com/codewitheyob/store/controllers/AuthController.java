b48a7adba5fc2b302ebadbdf264edb2fc38992ab
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-05-07 00:46:14 +0300

    feat: get current user

--- src/main/java/com/codewitheyob/store/controllers/AuthController.java
+++ src/main/java/com/codewitheyob/store/controllers/AuthController.java
@@ -2,6 +2,9 @@ package com.codewitheyob.store.controllers;
 
 import com.codewitheyob.store.dtos.JwtResponse;
 import com.codewitheyob.store.dtos.LoginRequest;
+import com.codewitheyob.store.dtos.UserDto;
+import com.codewitheyob.store.mappers.UserMapper;
+import com.codewitheyob.store.repositories.UserRepository;
 import com.codewitheyob.store.services.JwtService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
@@ -10,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -18,6 +22,8 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController {
     private final AuthenticationManager authenticationManager;
     private final JwtService jwtService;
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
 
     @PostMapping("/login")
     public ResponseEntity<JwtResponse> loginUser(@Valid @RequestBody LoginRequest request){
@@ -40,5 +46,16 @@ public class AuthController {
     public boolean validate(@RequestHeader("Authorization") String authHeader){
         var token = authHeader.replace("Bearer ", "");
         return jwtService.validateToken(token);
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<UserDto> me(){
+        var authentication = SecurityContextHolder.getContext().getAuthentication();
+        var email = (String) authentication.getPrincipal();
+
+        var user = userRepository.findByEmail(email).orElse(null);
+        if (user == null) return ResponseEntity.notFound().build();
+        var userDto = userMapper.toDto(user);
+        return ResponseEntity.ok(userDto);
     }
 }
