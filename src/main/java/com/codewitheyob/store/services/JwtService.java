166a7cd437b034d73a2ac1d84188628ae7609953
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-05-06 11:59:14 +0300

    feat: validate token

--- src/main/java/com/codewitheyob/store/services/JwtService.java
+++ src/main/java/com/codewitheyob/store/services/JwtService.java
@@ -1,20 +1,41 @@
 package com.codewitheyob.store.services;
 
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
 
 @Service
 public class JwtService {
+
+    @Value("${spring.jwt.secret}")
+    private String secret;
+
     public String generateToken(String email){
-        final long tokenExpiration = 86400; // 1 day
+        final long tokenExpiration = 86400; // 1 day (sec)
         return Jwts.builder()
                 .subject(email)
                 .issuedAt(new Date())
                 .expiration(new Date(System.currentTimeMillis() + 1000 * tokenExpiration))
-                .signWith(Keys.hmacShaKeyFor("secret".getBytes()))
+                .signWith(Keys.hmacShaKeyFor(secret.getBytes()))
                 .compact();
+    }
+
+    public boolean validateToken(String token){
+        try {
+            var claims = Jwts.parser()
+                    .verifyWith(Keys.hmacShaKeyFor(secret.getBytes()))
+                    .build()
+                    .parseSignedClaims(token)
+                    .getPayload();
+            return claims.getExpiration().after(new Date());
+
+        } catch (JwtException e) {
+            return false;
+        }
+
     }
 }
