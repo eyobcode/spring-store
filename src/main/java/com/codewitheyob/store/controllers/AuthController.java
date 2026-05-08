8dc30ebc10860bf262e28c43c8ff30e72a87887e
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-05-09 01:37:06 +0300

    Refactor: create jwt class as utils to help jwt service

--- src/main/java/com/codewitheyob/store/controllers/AuthController.java
+++ src/main/java/com/codewitheyob/store/controllers/AuthController.java
@@ -45,14 +45,14 @@ public class AuthController {
         var accessToken = jwtService.generateAccessToken(user);
         var refreshToken = jwtService.generateRefreshToken(user);
 
-        var cookie = new Cookie("refreshToken", refreshToken);
+        var cookie = new Cookie("refreshToken", refreshToken.toString());
         cookie.setHttpOnly(true);
         cookie.setPath("/auth/refresh");
         cookie.setMaxAge(jwtConfig.getRefreshTokenExpiration());
         cookie.setSecure(true);
         response.addCookie(cookie);
 
-        return ResponseEntity.ok(new JwtResponse(accessToken));
+        return ResponseEntity.ok(new JwtResponse(accessToken.toString()));
     }
 
     @ExceptionHandler(BadCredentialsException.class)
@@ -60,21 +60,15 @@ public class AuthController {
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
     }
 
-    @PostMapping("/validate")
-    public boolean validate(@RequestHeader("Authorization") String authHeader){
-        var token = authHeader.replace("Bearer ", "");
-        return jwtService.validateToken(token);
-    }
-
     @PostMapping("/refresh")
     public ResponseEntity<JwtResponse> refresh(@CookieValue(value = "refreshToken") String refreshToken){
-        if (!jwtService.validateToken(refreshToken)) return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        var jwt = jwtService.parseToken(refreshToken);
+        if (jwt == null || jwt.isExpired()) return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
 
-        var userId = jwtService.getUserIdFromToken(refreshToken);
-        var user = userRepository.findById(userId).orElseThrow();
+        var user = userRepository.findById(jwt.getUserId()).orElseThrow();
         var accessToken = jwtService.generateAccessToken(user);
 
-        return ResponseEntity.ok(new JwtResponse(accessToken));
+        return ResponseEntity.ok(new JwtResponse(accessToken.toString()));
     }
 
     @GetMapping("/me")
