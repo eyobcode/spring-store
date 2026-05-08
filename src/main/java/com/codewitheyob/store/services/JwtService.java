027aadaceb0eb1737f31add6e455c31c0faf7645
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-05-09 01:37:43 +0300

    Refactor: create jwt class as utils to help jwt service

--- src/main/java/com/codewitheyob/store/services/JwtService.java
+++ src/main/java/com/codewitheyob/store/services/JwtService.java
@@ -1,10 +1,8 @@
 package com.codewitheyob.store.services;
 
 import com.codewitheyob.store.config.JwtConfig;
-import com.codewitheyob.store.entities.Role;
 import com.codewitheyob.store.entities.User;
 import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -16,34 +14,33 @@ import java.util.Date;
 public class JwtService {
     private final JwtConfig jwtConfig;
 
-    public String generateAccessToken(User user){
+    public Jwt generateAccessToken(User user){
         return generateToken(user, jwtConfig.getAccessTokenExpiration());
     }
 
-    public String generateRefreshToken(User user){
+    public Jwt generateRefreshToken(User user){
         return generateToken(user, jwtConfig.getRefreshTokenExpiration());
     }
 
-    private String generateToken(User user, long tokenExpiration) {
-        return Jwts.builder()
-                .subject(user.getId().toString())
-                .claim("email", user.getEmail())
-                .claim("name", user.getName())
-                .claim("role", user.getRole())
+    private Jwt generateToken(User user, long tokenExpiration) {
+        var claims = Jwts.claims()
+                .add("email", user.getEmail())
+                .add("name", user.getName())
+                .add("role", user.getRole())
                 .issuedAt(new Date())
                 .expiration(new Date(System.currentTimeMillis() + 1000 * tokenExpiration))
-                .signWith(jwtConfig.getSecretKey())
-                .compact();
+                .build();
+
+        return new Jwt(claims, jwtConfig.getSecretKey());
     }
 
-    public boolean validateToken(String token){
+    public Jwt parseToken(String token){
         try {
             var claims = getClaims(token);
-            return claims.getExpiration().after(new Date());
-        } catch (JwtException e) {
-            return false;
+            return new Jwt(claims, jwtConfig.getSecretKey());
+        } catch (Exception e) {
+            return null;
         }
-
     }
 
     private Claims getClaims(String token) {
@@ -54,11 +51,4 @@ public class JwtService {
                 .getPayload();
     }
 
-    public Long getUserIdFromToken(String token){
-        return Long.valueOf(getClaims(token).getSubject());
-    }
-
-    public Role getUserRoleFromToken(String token) {
-        return Role.valueOf(getClaims(token).get("role", String.class));
-    }
 }
