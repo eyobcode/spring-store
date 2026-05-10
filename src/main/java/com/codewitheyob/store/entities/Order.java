5e693420634cbf1205f48ab9377ee492ee1989c1
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-05-10 03:05:18 +0300

    feat: add order and order item tables

--- src/main/java/com/codewitheyob/store/entities/Order.java
+++ src/main/java/com/codewitheyob/store/entities/Order.java
@@ -3,15 +3,12 @@ package com.codewitheyob.store.entities;
 import jakarta.persistence.*;
 import lombok.*;
 import java.math.BigDecimal;
-import java.time.Instant;
 import java.time.LocalDate;
 import java.util.LinkedHashSet;
-import java.util.List;
 import java.util.Set;
 
 @Getter
 @Setter
-@ToString
 @Entity
 @Table(name = "orders")
 public class Order {
