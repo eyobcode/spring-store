7e0e074b41865bdd0130baa9c1e93275e8701c91
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-05-10 03:04:45 +0300

    feat: add order and order item tables

--- src/main/java/com/codewitheyob/store/entities/Order.java
+++ src/main/java/com/codewitheyob/store/entities/Order.java
@@ -1,19 +1,16 @@
 package com.codewitheyob.store.entities;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import lombok.*;
-import org.hibernate.annotations.ColumnDefault;
-
 import java.math.BigDecimal;
 import java.time.Instant;
+import java.time.LocalDate;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
 
 @Getter
 @Setter
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder
 @ToString
 @Entity
 @Table(name = "orders")
@@ -22,18 +19,22 @@ public class Order {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne
     @ToString.Exclude
     @JoinColumn(name = "customer_id")
     private User customer;
 
     @Column(name = "status")
-    private String status;
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
 
-    @Column(name = "created_at")
-    private Instant createdAt;
+    @Column(name = "created_at",insertable = false,updatable = false)
+    private LocalDate createdAt;
 
     @Column(name = "total_price", nullable = false, precision = 10, scale = 2)
     private BigDecimal totalPrice;
+
+    @OneToMany(mappedBy = "order")
+    private Set<OrderItem> items = new LinkedHashSet<>();
 
 }
