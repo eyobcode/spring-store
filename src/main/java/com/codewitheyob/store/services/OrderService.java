bf6ee7e5c9aa60c2fb38e4eea92e292a93385290
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-05-14 23:17:15 +0300

    feat: get all customer orders

---
+++ src/main/java/com/codewitheyob/store/services/OrderService.java
@@ -0,0 +1,23 @@
+package com.codewitheyob.store.services;
+
+import com.codewitheyob.store.dtos.OrderDto;
+import com.codewitheyob.store.mappers.OrderMapper;
+import com.codewitheyob.store.repositories.OrderRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class OrderService {
+    private final AuthService authService;
+    private final OrderRepository orderRepository;
+    private final OrderMapper orderMapper;
+
+    public List<OrderDto> getAllOrders(){
+        var user = authService.getCurrentUser();
+        var orders = orderRepository.getAllByCustomer(user);
+        return orders.stream().map(orderMapper::toDto).toList();
+    }
+}
