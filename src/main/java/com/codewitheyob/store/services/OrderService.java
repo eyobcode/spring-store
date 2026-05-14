f12964d8dd51c431230d07a18e0b4a1e6a848c2f
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-05-15 00:06:08 +0300

    feat: get customer order

--- src/main/java/com/codewitheyob/store/services/OrderService.java
+++ src/main/java/com/codewitheyob/store/services/OrderService.java
@@ -1,12 +1,15 @@
 package com.codewitheyob.store.services;
 
 import com.codewitheyob.store.dtos.OrderDto;
+import com.codewitheyob.store.exceptions.OrderNotFoundException;
 import com.codewitheyob.store.mappers.OrderMapper;
 import com.codewitheyob.store.repositories.OrderRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 
 @Service
 @AllArgsConstructor
@@ -17,7 +20,19 @@ public class OrderService {
 
     public List<OrderDto> getAllOrders(){
         var user = authService.getCurrentUser();
-        var orders = orderRepository.getAllByCustomer(user);
+        var orders = orderRepository.getOrdersByCustomer(user);
         return orders.stream().map(orderMapper::toDto).toList();
+    }
+
+    public OrderDto getOrders(Long orderId) {
+        var order = orderRepository
+                .getOrderWithItems(orderId)
+                .orElseThrow(OrderNotFoundException::new);
+        var user = authService.getCurrentUser();
+
+        if (!order.isPlacedBy(user))
+            throw new AccessDeniedException("You don't have access to this order.");
+
+        return orderMapper.toDto(order);
     }
 }
