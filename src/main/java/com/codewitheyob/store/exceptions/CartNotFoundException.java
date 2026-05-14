c74d2342e66941f21b6720e5ab575066376e4331
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-05-14 22:18:52 +0300

    refactor: cut the business logic out from checkout controller and create cart empty exception class with error handler.

--- src/main/java/com/codewitheyob/store/exceptions/CartNotFoundException.java
+++ src/main/java/com/codewitheyob/store/exceptions/CartNotFoundException.java
@@ -1,4 +1,7 @@
 package com.codewitheyob.store.exceptions;
 
 public class CartNotFoundException extends RuntimeException{
+    public CartNotFoundException(){
+        super("Cart not found.");
+    }
 }
