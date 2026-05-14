2211b29a6df1766cf3d3bcb53e46f09edde75184
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-05-14 23:17:12 +0300

    feat: get all customer orders

--- src/main/java/com/codewitheyob/store/repositories/OrderRepository.java
+++ src/main/java/com/codewitheyob/store/repositories/OrderRepository.java
@@ -1,7 +1,16 @@
 package com.codewitheyob.store.repositories;
 
 import com.codewitheyob.store.entities.Order;
-import org.springframework.data.repository.CrudRepository;
+import com.codewitheyob.store.entities.User;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface OrderRepository extends CrudRepository<Order, Long> {
+import java.util.List;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+    @EntityGraph(attributePaths = "items.product")
+    @Query("SELECT o FROM Order o WHERE o.customer= :customer")
+    List<Order> getAllByCustomer(@Param("customer") User customer);
 }
