ab1d4276a0a41be45971520118a74a83e79e7d3a
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-05-15 16:18:28 +0300

    feat: add stripe to the project.

--- src/main/java/com/codewitheyob/store/services/CheckoutService.java
+++ src/main/java/com/codewitheyob/store/services/CheckoutService.java
@@ -7,18 +7,29 @@ import com.codewitheyob.store.exceptions.CartNotFoundException;
 import com.codewitheyob.store.exceptions.CartEmptyException;
 import com.codewitheyob.store.repositories.CartRepository;
 import com.codewitheyob.store.repositories.OrderRepository;
-import lombok.AllArgsConstructor;
+import com.stripe.exception.StripeException;
+import com.stripe.model.checkout.Session;
+import com.stripe.param.checkout.SessionCreateParams;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
 
 @Service
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class CheckoutService {
     private final CartRepository cartRepository;
     private final OrderRepository orderRepository;
     private final AuthService authService;
     private final CartService cartService;
 
-    public CheckoutResponse checkout(CheckoutRequest request){
+    @Value("${websiteUrl}")
+    private String websiteUrl;
+
+    @Transactional
+    public CheckoutResponse checkout(CheckoutRequest request) throws StripeException {
         var cart = cartRepository.getCartWithItems(request.getCartId()).orElse(null);
         if (cart == null) throw new CartNotFoundException();
 
@@ -27,8 +38,39 @@ public class CheckoutService {
         var order = Order.formatCart(cart,authService.getCurrentUser());
 
         orderRepository.save(order);
-        cartService.clearCart(cart.getId());
 
-        return new CheckoutResponse(order.getId());
+        try {
+            // Create checkout session
+            var builder =  SessionCreateParams.builder()
+                    .setMode(SessionCreateParams.Mode.PAYMENT)
+                    .setSuccessUrl(websiteUrl + "/checkout-success?orderId=" + order.getId())
+                    .setCancelUrl(websiteUrl + "/checkout-cancel");
+
+            order.getItems().forEach(item -> {
+                var lineItem =  SessionCreateParams.LineItem.builder()
+                        .setQuantity(Long.valueOf(item.getQuantity()))
+                        .setPriceData(
+                                SessionCreateParams.LineItem.PriceData.builder()
+                                        .setUnitAmountDecimal(
+                                                item.getUnitPrice()
+                                                        .multiply(BigDecimal.valueOf(100)))
+                                        .setCurrency("usd")
+                                        .setProductData(
+                                                SessionCreateParams.LineItem.PriceData.ProductData.builder()
+                                                        .setName(item.getProduct().getName()).build()
+                                        ).build()
+                        ).build();
+                builder.addLineItem(lineItem);
+            });
+
+            var session = Session.create(builder.build());
+
+            cartService.clearCart(cart.getId());
+
+            return new CheckoutResponse(order.getId(),session.getUrl());
+        } catch (StripeException ex){
+            orderRepository.delete(order);
+            throw ex;
+        }
     }
 }
