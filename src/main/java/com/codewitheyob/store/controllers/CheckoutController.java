4d2a37883cc3a83bd300587256341a01dad426e5
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-05-15 16:18:23 +0300

    feat: add stripe to the project.

--- src/main/java/com/codewitheyob/store/controllers/CheckoutController.java
+++ src/main/java/com/codewitheyob/store/controllers/CheckoutController.java
@@ -1,13 +1,13 @@
 package com.codewitheyob.store.controllers;
 
 import com.codewitheyob.store.dtos.CheckoutRequest;
-import com.codewitheyob.store.dtos.CheckoutResponse;
 import com.codewitheyob.store.dtos.ErrorDto;
 import com.codewitheyob.store.exceptions.CartNotFoundException;
 import com.codewitheyob.store.exceptions.CartEmptyException;
 import com.codewitheyob.store.services.CheckoutService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,8 +19,15 @@ public class CheckoutController {
     private final CheckoutService checkoutService;
 
     @PostMapping
-    public CheckoutResponse checkout(@Valid @RequestBody CheckoutRequest request){
-        return checkoutService.checkout(request);
+    public ResponseEntity<?> checkout(@Valid @RequestBody CheckoutRequest request){
+        try {
+            return ResponseEntity.ok(checkoutService.checkout(request));
+        } catch (Exception ex){
+            System.out.println(ex.getMessage());
+            return ResponseEntity
+                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new ErrorDto("Error creating a checkout session"));
+        }
     }
 
     @ExceptionHandler({CartNotFoundException.class, CartEmptyException.class})
