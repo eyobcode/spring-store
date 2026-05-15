f94e2a7dac1b0ff060b03d74b7a53dc69fa10dac
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-05-15 16:57:17 +0300

    feat: decoupling stripe and encapsulate it

---
+++ src/main/java/com/codewitheyob/store/services/StripePaymentGateway.java
@@ -0,0 +1,59 @@
+package com.codewitheyob.store.services;
+
+import com.codewitheyob.store.entities.Order;
+import com.codewitheyob.store.entities.OrderItem;
+import com.codewitheyob.store.exceptions.PaymentException;
+import com.stripe.exception.StripeException;
+import com.stripe.model.checkout.Session;
+import com.stripe.param.checkout.SessionCreateParams;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service
+public class StripePaymentGateway implements PaymentGateway {
+
+    @Value("${websiteUrl}")
+    private String websiteUrl;
+
+    @Override
+    public CheckoutSession createCheckoutSession(Order order) {
+        try {
+            var builder =  SessionCreateParams.builder()
+                    .setMode(SessionCreateParams.Mode.PAYMENT)
+                    .setSuccessUrl(websiteUrl + "/checkout-success?orderId=" + order.getId())
+                    .setCancelUrl(websiteUrl + "/checkout-cancel");
+
+            order.getItems().forEach(item -> {
+                var lineItem = createLineItem(item);
+                builder.addLineItem(lineItem);
+            });
+            var session = Session.create(builder.build());
+            return new CheckoutSession(session.getUrl());
+        } catch (StripeException e) {
+            throw new PaymentException();
+        }
+    }
+
+    private SessionCreateParams.LineItem createLineItem(OrderItem item) {
+        return SessionCreateParams.LineItem.builder()
+                .setQuantity(Long.valueOf(item.getQuantity()))
+                .setPriceData(createPriceData(item))
+                .build();
+    }
+
+    private SessionCreateParams.LineItem.PriceData createPriceData(OrderItem item) {
+        return SessionCreateParams.LineItem.PriceData.builder()
+                .setUnitAmountDecimal(
+                        item.getUnitPrice().multiply(BigDecimal.valueOf(100)))
+                .setCurrency("usd")
+                .setProductData(createProductData(item))
+                .build();
+    }
+
+    private SessionCreateParams.LineItem.PriceData.ProductData createProductData(OrderItem item) {
+        return SessionCreateParams.LineItem.PriceData.ProductData.builder()
+                .setName(item.getProduct().getName()).build();
+    }
+}
