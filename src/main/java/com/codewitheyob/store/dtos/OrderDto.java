4b95a5b7d6c611530ef8605d11138a3bc4aa401d
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-05-16 02:57:55 +0300

    feat: decoupling stripe and encapsulate it make generic/abstruction

--- src/main/java/com/codewitheyob/store/dtos/OrderDto.java
+++ src/main/java/com/codewitheyob/store/dtos/OrderDto.java
@@ -1,6 +1,6 @@
 package com.codewitheyob.store.dtos;
 
-import com.codewitheyob.store.entities.OrderStatus;
+import com.codewitheyob.store.entities.PaymentStatus;
 import lombok.Data;
 
 import java.math.BigDecimal;
@@ -10,7 +10,7 @@ import java.util.List;
 @Data
 public class OrderDto {
     private Long id;
-    private OrderStatus status;
+    private PaymentStatus status;
     private LocalDate createdAt;
     private BigDecimal totalPrice;
     private List<OrderItemDto> items;
