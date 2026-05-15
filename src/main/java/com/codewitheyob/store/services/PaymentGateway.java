27259dabb4c6c8378bee53c8f4e01ff01d9031d6
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-05-16 02:58:01 +0300

    feat: decoupling stripe and encapsulate it make generic/abstruction

--- src/main/java/com/codewitheyob/store/services/PaymentGateway.java
+++ src/main/java/com/codewitheyob/store/services/PaymentGateway.java
@@ -2,6 +2,9 @@ package com.codewitheyob.store.services;
 
 import com.codewitheyob.store.entities.Order;
 
+import java.util.Optional;
+
 public interface PaymentGateway {
     CheckoutSession createCheckoutSession(Order order);
+    Optional<PaymentResult> parseWebhookRequest(WebhookRequest request);
 }
