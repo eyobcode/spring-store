28f42859885b3249082b20291d959858389fb4fc
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-05-16 02:57:46 +0300

    feat: decoupling stripe and encapsulate it make generic/abstruction

--- src/main/java/com/codewitheyob/store/controllers/CheckoutController.java
+++ src/main/java/com/codewitheyob/store/controllers/CheckoutController.java
@@ -6,23 +6,36 @@ import com.codewitheyob.store.dtos.ErrorDto;
 import com.codewitheyob.store.exceptions.CartNotFoundException;
 import com.codewitheyob.store.exceptions.CartEmptyException;
 import com.codewitheyob.store.exceptions.PaymentException;
+import com.codewitheyob.store.repositories.OrderRepository;
 import com.codewitheyob.store.services.CheckoutService;
+import com.codewitheyob.store.services.WebhookRequest;
 import jakarta.validation.Valid;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
+
 
 @RestController
-@AllArgsConstructor
+@RequiredArgsConstructor
 @RequestMapping("/checkout")
 public class CheckoutController {
     private final CheckoutService checkoutService;
+    private final OrderRepository orderRepository;
 
     @PostMapping
-    public CheckoutResponse checkout(@Valid @RequestBody CheckoutRequest request){
+    public CheckoutResponse checkout(@Valid @RequestBody CheckoutRequest request) {
         return checkoutService.checkout(request);
+    }
+
+    @PostMapping("/webhook")
+    public void handleWebhook(
+            @RequestHeader Map<String,String> header,
+            @RequestBody String payload) {
+
+        checkoutService.handleWebhookEvent(new WebhookRequest(header, payload));
     }
 
     @ExceptionHandler(PaymentException.class)
