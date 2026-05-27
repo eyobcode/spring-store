e0d365289891d440ae09c51d74487b45f90d1eaa
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-05-27 23:45:34 +0300

    feat: decoupling stripe and encapsulate it make generic/abstruction

--- src/main/java/com/codewitheyob/store/mappers/OrderMapper.java
+++ src/main/java/com/codewitheyob/store/mappers/OrderMapper.java
@@ -4,7 +4,6 @@ import com.codewitheyob.store.dtos.OrderDto;
 import com.codewitheyob.store.entities.Order;
 import org.mapstruct.Mapper;
 
-import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface OrderMapper {
